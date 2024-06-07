4013d65defb7ffbd8dab62d386e2c87dfba4130f
AGAT89 <aariastevez@gmail.com>
2024-06-06 23:32:04 -0500

    Agat

--- src/app/modules/empleados/empleados.component.ts
+++ src/app/modules/empleados/empleados.component.ts
@@ -67,76 +67,7 @@ export class EmpleadosComponent implements OnInit {
   ]
 
   productsList = [
-    {
-        id: 31,
-        name: 'Gray Sofa',
-        avatar: 'assets/images/others/thumb-9.jpg',
-        category: 'Home Decoration',
-        price: 912,
-        quantity: 23,
-        status: 'in stock',
-        checked : false
-    },
-    {
-        id: 32,
-        name: 'Beat Headphone',
-        avatar: 'assets/images/others/thumb-10.jpg',
-        category: 'Eletronic',
-        price: 137,
-        quantity: 56,
-        status: 'in stock',
-        checked : false
-    },
-    {
-        id: 33,
-        name: 'Wooden Rhino',
-        avatar: 'assets/images/others/thumb-11.jpg',
-        category: 'Home Decoration',
-        price: 912,
-        quantity: 12,
-        status: 'in stock',
-        checked : false
-    },
-    {
-        id: 34,
-        name: 'Red Chair',
-        avatar: 'assets/images/others/thumb-12.jpg',
-        category: 'Home Decoration',
-        price: 128,
-        quantity: 0,
-        status: 'out of stock',
-        checked : false
-    },
-    {
-        id: 35,
-        name: 'Wristband',
-        avatar: 'assets/images/others/thumb-13.jpg',
-        category: 'Eletronic',
-        price: 776,
-        quantity: 0,
-        status: 'out of stock',
-        checked : false
-    },
-    {
-        id: 36,
-        name: 'Charging Cable',
-        avatar: 'assets/images/others/thumb-14.jpg',
-        category: 'Eletronic',
-        price: 119,
-        quantity: 37,
-        status: 'in stock',
-        checked : false
-    },
-    {
-        id: 37,
-        name: 'Three Legs',
-        avatar: 'assets/images/others/thumb-15.jpg',
-        category: 'Home Decoration',
-        price: 199,
-        quantity: 17,
-        status: 'in stock',
-        checked : false
-    },
+   
   ]
 
   isVisible = false;
